42658b2dcc58ba6e4c30906d10e7de492df78f73
Tyler Robinson <tyler.robinson.business@gmail.com>
2021-10-31 21:40:16 -0400

    commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://TRCoder:dbpassword@messagefromdownunder.xv6xa.mongodb.net/Messagefromdownunder?retryWrites=true&w=majority', 
+    'petData': 'dietData'
+};
